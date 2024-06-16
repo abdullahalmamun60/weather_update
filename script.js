42a5b31d6a5006e2c2a62f5f26567218478b4ada
Abdullah Al Mamun <mamuneteruet@gmai.com>
2024-06-17 01:46:23 +0600

    tried but need to be fixed more

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 // http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={faac80e76b01a96478dcdf62b8524853}
-const api_key = 'faac80e76b01a96478dcdf62b8524853' ;
-const city = 'Dhaka'
+
+// fetch(url)
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data);
+//     });
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const searchTemperature = () => {
+    const city = document.getElementById("city-name").value
+    const api_key = 'faac80e76b01a96478dcdf62b8524853';
+// const city = 'Dhaka'
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`
+ fetch (url)
+ .then (res => res.json())
+ .then (data => displayTemperature(data))
+}
 
-fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-    });
-
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp)
+    setInnerText('condition', temperature.weather[0].main)
+    //icon setter
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('icon');
+    imgIcon.setAttribute('src', url);
+}
